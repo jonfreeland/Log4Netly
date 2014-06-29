8ae106218fd273b8c2c9b41243678a8b616419cf
Jonathan Freeland <jon.freeland@gmail.com>
2014-06-28 23:32:36 -0400

    Initial commit.

---
+++ Log4Netly/LogglyAppender.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Net.Http;
+using log4net.Appender;
+using log4net.Core;
+using Newtonsoft.Json;
+
+namespace Log4Netly {
+    public class LogglyAppender : AppenderSkeleton {
+        private readonly Process _currentProcess = Process.GetCurrentProcess();
+        private string _endpoint;
+
+        /// <summary>
+        /// Loggly endpoint for submitting log events.
+        /// </summary>
+        public string Endpoint {
+            get { return _endpoint; }
+            set {
+                _endpoint = value;
+                if (!_endpoint.EndsWith("/"))
+                    _endpoint += "/";
+                if (!_endpoint.EndsWith("inputs/"))
+                    _endpoint += "inputs/";
+            }
+        }
+
+        /// <summary>
+        /// Loggly customer token.
+        /// https://www.loggly.com/docs/customer-token-authentication-token/
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Comma-delimited list of tags to add to each log event.
+        /// https://www.loggly.com/docs/tags/
+        /// </summary>
+        public string Tags { get; set; }
+
+        public LogglyAppender() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tags">Collection of additional tags to add to each log event.</param>
+        public LogglyAppender(IEnumerable<string> tags) {
+            if (!string.IsNullOrWhiteSpace(Tags))
+                Tags += ",";
+
+            Tags += string.Join(",", tags);
+        }
+
+        protected override void Append(LoggingEvent loggingEvent) {
+            dynamic payload = new ExpandoObject();
+            payload.level = loggingEvent.Level.DisplayName;
+            payload.time = loggingEvent.TimeStamp.ToString("o");
+            payload.machine = Environment.MachineName;
+            payload.process = _currentProcess.ProcessName;
+            payload.thread = loggingEvent.ThreadName;
+            payload.message = loggingEvent.RenderedMessage;
+            payload.logger = loggingEvent.LoggerName;
+
+            var exception = loggingEvent.ExceptionObject;
+            if (exception != null) {
+                payload.exception = new ExpandoObject();
+                payload.exception.message = exception.Message;
+                payload.exception.stackTrace = exception.StackTrace;
+                if (exception.InnerException != null) {
+                    payload.exception.innerException = new ExpandoObject();
+                    payload.exception.innerException.message = exception.InnerException.Message;
+                    payload.exception.innerException.stackTrace = exception.InnerException.StackTrace;
+                }
+            }
+
+            var client = new HttpClient();
+            var url = string.Format("{0}{1}/tag/{2}", Endpoint, Token, Tags);
+            var payloadJson = JsonConvert.SerializeObject(payload);
+            var response = client.PostAsync(url, new StringContent(payloadJson));
+        }
+    }
+}
---
+++ Log4NetlyTesting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Log4NetlyTesting {
+    class Program {
+        static void Main(string[] args) {
+            log4net.Config.XmlConfigurator.Configure();
+            var logger = LogManager.GetLogger(typeof(Program));
+            ////logger.Info("I know he can get the job, but can he do the job?");
+            ////logger.Debug("I'm not arguing that with you.");
+            ////logger.Warn("Be careful!");
+
+            logger.Error("Now you've done it...", new Exception("This is my exception message.", new Exception("This is my inner exception message.")));
+            try {
+                var hi = 1/int.Parse("0");
+            } catch (Exception ex) {
+                logger.Error("Now you've done it...", ex);
+            }
+            ////logger.Fatal("That's it. It's over.");
+        }
+    }
+}
